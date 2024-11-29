8fe560181812dbd31424d6cc2c1778ec8fe8cbee
reoreo-zyt <768119359@qq.com>
2024-11-29 19:12:32 +0800

    feat: 修复

--- src/components/MPaper.tsx
+++ src/components/MPaper.tsx
@@ -217,7 +217,7 @@ export default function MPaper() {
             onMouseDown={(event) => handleClickOpen(event, index)}
           >
             <img className="img" alt={item.alt} src={item.src} />
-            {item.show ? (
+            {/* {item.show ? (
               <animated.div className="text" style={{ ...springs }}>
                 {item.text}
               </animated.div>
@@ -225,7 +225,7 @@ export default function MPaper() {
               <div className="text" style={{}}>
                 {item.text}
               </div>
-            ) : null}
+            ) : null} */}
           </Paper>
         ))}
       </div>
